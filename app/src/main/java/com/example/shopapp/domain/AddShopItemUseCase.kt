19dd3a7b92d250a62c4e6d90ac7947839de17a37
adizqx <nerozqx@gmail.com>
2022-05-31 10:53:02 +0600

    activity

---
+++ app/src/main/java/com/example/shopapp/domain/AddShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shopapp.domain
+
+class AddShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun addShopItem(shopItem: ShopItem) {
+        shopListRepository.addShopItem(shopItem)
+    }
+}
---
+++ app/src/main/java/com/example/shopapp/presentation/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.shopapp.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shopapp.data.ShopListRepositoryImpl
+import com.example.shopapp.domain.DeleteShopItemUseCase
+import com.example.shopapp.domain.EditShopItemUseCase
+import com.example.shopapp.domain.GetShopListUseCase
+import com.example.shopapp.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+    }
+}
